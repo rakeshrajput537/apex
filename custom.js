2054e94b4bee189609a8bd74670ac15a0be93033
rakesh kumar <rakeshrajput537@gmail.com>
2019-02-03 20:24:03 +0530

    done

--- custom.js
+++ custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
     $('.customer-logos').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
